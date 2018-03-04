681352ceb475591a37362b3d4e1de99455db82a2
SlusariucDan <dan.slusariuc@info.uaic.ro>
2018-03-05 01:43:53 +0200

    Local commit

--- src/assetManager/SortByValueAssets.java
+++ src/assetManager/SortByValueAssets.java
@@ -7,6 +7,8 @@ import java.util.Comparator;
 public class SortByValueAssets extends AssetManager implements Comparator<Asset> {
     @Override
     public int compare(Asset o1, Asset o2) {
-        return (int)(o1.computeProfit()-o2.computeProfit());
+        if(o1.computeProfit() < o2.computeProfit())
+            return -1;
+        return 1;
     }
 }
