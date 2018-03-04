af10664f2d2fe79eb1913d6477a54e355e0d63c2
chircancristi <cristianchircan1@gmail.com>
2018-03-04 16:27:26 +0200

    Optional part 1

--- src/assetManager/AssetManager.java
+++ src/assetManager/AssetManager.java
@@ -1,6 +1,8 @@
 package assetManager;
 
+import assets.Assets;
 import item.Item;
+import jewel.Jewel;
 import vehicle.Vehicle;
 
 import java.util.ArrayList;
@@ -8,15 +10,18 @@ import java.util.List;
 
 public class AssetManager {
     List<Item> items ;
-
+    List<Assets> assets;
     public AssetManager() {
             this.items=new ArrayList<>();
+            this.assets=new ArrayList<>();
     }
     public void add (Item... Items)
     {
 
-        for (Item i : Items)
-                this.items.add(i);
+        for (Item i : Items) {
+            this.items.add(i);
+            if (i instanceof Jewel==false) assets.add((Assets)i);
+        }
     }
     public List<Item> getItems()
     {
@@ -52,4 +57,26 @@ public class AssetManager {
     }
 
 
+    public String getAssets() {
+        int i=0,numberOfAssets=0;
+        float []result = new float[100];
+        for (i=0;i<this.assets.size();i++){
+            result[numberOfAssets]=assets.get(i).computeProfit();
+            numberOfAssets++;
         }
+        float aux=0;
+        int j;
+        String stringResult = new String();
+        for (i=0;i<numberOfAssets;i++)
+            for (j=0;j<numberOfAssets;j++)
+                    if (result[i]<result[j])
+                    {
+                        aux=result[i];
+                        result[i]=result[j];
+                        result[j]=aux;
+                    }
+        for (i=0;i<numberOfAssets;i++)
+               stringResult=stringResult+ String.valueOf(result[i]) + ' ';
+        return stringResult;
+    }
+}
